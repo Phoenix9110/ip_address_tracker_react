4fa0c4173da5bae5be83d8c932a333926020f943
Phoenix9110 <claus9110@gmail.com>
2024-03-26 11:41:30 -0500

    added mobile design

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import { ipStore } from "@/store/IPstore"
+import * as L from "leaflet";
+
+var Icon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 
-const Maps: React.FC<UserProps> = () => {
+const Maps: React.FC = () => {
   const ipInformation = ipStore((state:any) => state.ipInformation)
-  const coordinates = ipInformation.filter((property)=> property.item === "coordinates")
-  const center = coordinates[0]?.content 
+  const coordinates = ipInformation.filter((property:any)=> property?.item === "coordinates")
+  const center = coordinates[0]?.content || [ 4.541, -74.05506 ]
 
-
-  console.log(coordinates)
   return (
 
-      <MapContainer center={center} zoom={5} scrollWheelZoom={false} style={{ height: '100vh', width: '100wh' }} key={center.toString()}>
+      <MapContainer center={center} zoom={6} scrollWheelZoom={false} style={{ height: '100vh', width: '100wh' }} key={center.toString()}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        <Marker position={center}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={center}  icon={Icon}>
         </Marker>
       </MapContainer>
   );
